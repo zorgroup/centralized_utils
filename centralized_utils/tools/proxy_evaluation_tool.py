e2da394e948341459303176fc2c9b709e848222a
Murtaza <murtazamail15@gmail.com>
2025-07-02 05:30:31 +0500

    removed SessionController because proxies didn't rotate, added success rate

--- centralized_utils/tools/proxy_evaluation_tool.py
+++ centralized_utils/tools/proxy_evaluation_tool.py
@@ -2,8 +2,9 @@ import os
 import redis
 import random
 import asyncio
-from curl_cffi import requests
+import aiohttp
 import traceback
+from datetime import datetime
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -41,94 +42,96 @@ def load_proxy_subscriptions(redis_client):
 
 
 
-async def fetch_ip_info(session, gateway_url, semaphore):
+async def fetch_ip_info(gateway_url, semaphore):
     async with semaphore:
-        while True:    # Keep retrying until response is received.
-            try:               
-                response = await session.get(
-                    f'https://ipinfo.io/json?val={random.randint(1,10000)}', 
-                    proxy=gateway_url, 
-                    timeout=15, 
-                    verify=False, 
-                    impersonate='chrome131'
-                )
-                if response.status_code != 200:
-                    raise Exception(f'Invalid status: {response.status_code}')
-                data = response.json()
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'https://ipinfo.io/json?val={random.randint(1,10000)}', proxy=gateway_url, timeout=15, ssl=False) as response:
+                if response.status != 200:
+                    raise Exception(f'Invalid status: {response.status}')
+                data = await response.json()
                 ip =  data['ip']
-                subnet = '.'.join(data['ip'].split('.')[0:3]),
+                subnet = '.'.join(data['ip'].split('.')[0:3])
                 country = data['country']
                 return ip, subnet, country
-            except Exception as e:
-                print(f'\nERROR ----------- {gateway_url}')
-                #print(str(e))
-                #traceback.print_exc()
-                await asyncio.sleep(5)
         
 
 
 async def evaluate_proxy_subscription(subscription_id: str, ip_gateways: list, num_requests_to_test: int, concurrency: int):
     # Create stats
-
     request_count = 0
+    success_count = 0
+    error_count = 0
     ip_pool = set()
     subnet_pool = set()
     ip_country_counters = {
         'US': 0,
         'Worldwide': 0
     }
+    start_time = datetime.now()
 
 
     print(f'\n\nEvaluating proxy id: {subscription_id}')
-    async with requests.AsyncSession() as session:           # Using a persistent session results in much higher success rate for some vendors like geonode. Reason unknown.
-        tasks = []
-        semaphore = asyncio.Semaphore(concurrency)
-        for _ in range(num_requests_to_test):
-            gateway_url = f'http://' + random.choice(ip_gateways)
-            tasks.append(fetch_ip_info(session, gateway_url, semaphore))
+    semaphore = asyncio.Semaphore(concurrency)
+    tasks = []
+    for _ in range(num_requests_to_test):
+        gateway_url = f'http://' + random.choice(ip_gateways)
+        tasks.append(fetch_ip_info(gateway_url, semaphore))
 
-        for i, task in enumerate(asyncio.as_completed(tasks), 1):
-                ip, subnet, country = await task
-                
-                # Increment reqesut count.
-                request_count += 1
+    for i, task in enumerate(asyncio.as_completed(tasks), 1):
+        request_count += 1
+        try:
+            ip, subnet, country = await task
+            success_count += 1
 
-                # Increment country counts (before saving ip).
-                if ip not in ip_pool:
-                    if country == 'US':
-                        ip_country_counters['US'] += 1
-                    else:
-                        ip_country_counters['Worldwide'] += 1
+            # Increment country counts (before saving ip).
+            if ip not in ip_pool:
+                if country == 'US':
+                    ip_country_counters['US'] += 1
+                else:
+                    ip_country_counters['Worldwide'] += 1
 
-                # Save ip and subnet.
-                ip_pool.add(ip)
-                subnet_pool.add(subnet)
-
-                # Print results periodically.
-                if request_count % 10 == 0:
-                    print(f'\nProxy ID:        {subscription_id}')
-                    print(f"Request Count:   {request_count:,}")
-                    print(f"Unique IPs:      {len(ip_pool):,}")
-                    print(f"Unique Subnets:  {len(subnet_pool):,}")
-                    print(f"IP Countries:    {dict(ip_country_counters)}")
+            # Save ip and subnet.
+            ip_pool.add(ip)
+            subnet_pool.add(subnet)
+        
+        except:
+            error_count += 1
+        
+        finally:        
+            # Print results periodically.
+            if request_count % 10 == 0:
+                print(f'\nProxy ID:        {subscription_id}')
+                print(f"Request Count:   {request_count:,}")
+                print(f"Success Count:   {success_count:,}")
+                print(f"Success Rate:    {(success_count / request_count) * 100:,.1f} %")
+                print(f"Unique IPs:      {len(ip_pool):,}")
+                print(f"Unique Subnets:  {len(subnet_pool):,}")
+                print(f"IP Countries:    {dict(ip_country_counters)}")
+                print(f"Time Taken:      {str(datetime.now() - start_time).split('.')[0]}")
            
 
     # Print results at end of evaluation.
     print("\n\n------------------------------------------------------ Proxy Final Summary ------------------------------------------------------")
     print(f'Proxy ID:        {subscription_id}')
     print(f"Request Count:   {request_count:,}")
+    print(f"Success Count:   {success_count:,}")
+    print(f"Success Rate:    {(success_count / request_count) * 100:,.1f} %")
     print(f"Unique IPs:      {len(ip_pool):,}")
     print(f"Unique Subnets:  {len(subnet_pool):,}")
-    print(f"IP Countries:    {dict(ip_country_counters)}\n")
+    print(f"IP Countries:    {dict(ip_country_counters)}")
+    print(f"Time Taken:      {str(datetime.now() - start_time).split('.')[0]}")
     print("---------------------------------------------------------------------------------------------------------------------------------")
 
     # Write results to file.
     with open('proxy_evaluation_results.txt', 'a', encoding='utf-8') as f:
         f.write(f'Proxy ID:        {subscription_id}\n')
         f.write(f"Request Count:   {request_count:,}\n")
+        f.write(f"Success Count:   {success_count:,}\n")
+        f.write(f"Success Rate:    {(success_count / request_count) * 100:,.1f} %\n")
         f.write(f"Unique IPs:      {len(ip_pool):,}\n")
         f.write(f"Unique Subnets:  {len(subnet_pool):,}\n")
-        f.write(f"IP Countries:    {dict(ip_country_counters)}\n\n\n\n")
+        f.write(f"IP Countries:    {dict(ip_country_counters)}\n")
+        f.write(f"Time Taken:      {str(datetime.now() - start_time).split('.')[0]}\n\n\n\n")
 
 
 
@@ -155,7 +158,7 @@ async def main():
             evaluate_proxy_subscription(
                 subscription_id=subscription_id, 
                 ip_gateways=ip_gateways,
-                num_requests_to_test=10_000,
+                num_requests_to_test=100,
                 concurrency=5
             )
         )
