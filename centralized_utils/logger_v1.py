7f079e169aeea6dffa9336e65b8a33bf5087f080
Murtaza <murtazamail15@gmail.com>
2025-07-23 22:52:05 +0500

    bug fix for missing in_stock in log_products

--- centralized_utils/logger_v1.py
+++ centralized_utils/logger_v1.py
@@ -240,9 +240,9 @@ class LogController:
         for product in products:
             product_info = {
                 'product_url': product['product_url'],
-                'price': product['price'] if 'price' in product else None,
-                'in_stock': product['in_stock'],
-                'currency': product['currency'] if 'currency' in product else None
+                'price': product['price'] if 'price' in product else 'missing',
+                'in_stock': product['in_stock'] if 'in_stock' in product else 'missing',
+                'currency': product['currency'] if 'currency' in product else 'missing'
             }
             self.logger.info(f'product: {json.dumps(product_info)}')
 
